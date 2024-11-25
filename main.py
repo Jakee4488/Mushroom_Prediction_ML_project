4b529c72160206cce018b81883cdd003e4e8a570
Jakee4488 <111643447+Jakee4488@users.noreply.github.com>
2024-11-25 12:38:48 +0000

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from GDP_Prediction import logger
+from GDP_Prediction.pipeline.Data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Hello World")
+#
+STAGE_NAME="Data Ingestion"
+
+try:
+        logger.info(f"Running {STAGE_NAME} stage")
+        ingestion_pipeline = DataIngestionTrainingPipeline()
+        ingestion_pipeline.main()
+        logger.info(f"Completed {STAGE_NAME} stage")
+except Exception as e:  
+        logger.error(f"Failed to run {STAGE_NAME} stage")
+        logger.error(e)
+        raise e
--- src/GDP_Prediction/utils/common.py
+++ src/GDP_Prediction/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from GDP_Prediction import logger
 import json
 import joblib
 from ensure import ensure_annotations
