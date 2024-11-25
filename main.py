c884d9f414e6fa074c5820623c158b285207b5c6
Jakee4488 <111643447+Jakee4488@users.noreply.github.com>
2024-11-25 23:15:03 +0000

    Model Training added to the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from GDP_Prediction import logger
 from GDP_Prediction.pipeline.Data_ingestion import DataIngestionTrainingPipeline
 from GDP_Prediction.pipeline.Data_Validation import DataValidationTrainingPipeline
 from GDP_Prediction.pipeline.Data_Transformation import DataTransformationTrainingPipeline
+from GDP_Prediction.pipeline.Model_Trainer import ModelTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -32,6 +33,19 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/GDP_Prediction/pipeline/Model_Trainer.py
@@ -0,0 +1,30 @@
+from GDP_Prediction.config.configuration import ConfigurationManager
+from GDP_Prediction.components.model_trainer import ModelTrainer
+from GDP_Prediction import logger
+
+
+
+STAGE_NAME = "Model Trainer stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
