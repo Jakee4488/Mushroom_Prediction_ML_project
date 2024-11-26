bf55d770ed62274a014ddbc776784e835b919e5a
Jakee4488 <111643447+Jakee4488@users.noreply.github.com>
2024-11-26 17:54:53 +0000

    appended flask routes and HTML

--- app.py
+++ app.py
@@ -1,57 +1,61 @@
+# app.py
+
 from flask import Flask, render_template, request
-import os 
 import numpy as np
-import pandas as pd
 from GDP_Prediction.pipeline.prediction import PredictionPipeline
 
+app = Flask(__name__)
 
-app = Flask(__name__) # initializing a flask app
-
-@app.route('/',methods=['GET'])  # route to display the home page
+@app.route('/', methods=['GET'])
 def homePage():
     return render_template("index.html")
 
-
-@app.route('/train',methods=['GET'])  # route to train the pipeline
-def training():
-    os.system("python main.py")
-    return "Training Successful!" 
-
-
-@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
-def index():
+@app.route('/predict', methods=['POST'])
+def predict():
     if request.method == 'POST':
         try:
-            #  reading the inputs given by the user
-            fixed_acidity =float(request.form['fixed_acidity'])
-            volatile_acidity =float(request.form['volatile_acidity'])
-            citric_acid =float(request.form['citric_acid'])
-            residual_sugar =float(request.form['residual_sugar'])
-            chlorides =float(request.form['chlorides'])
-            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
-            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
-            density =float(request.form['density'])
-            pH =float(request.form['pH'])
-            sulphates =float(request.form['sulphates'])
-            alcohol =float(request.form['alcohol'])
-       
-         
-            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
-            data = np.array(data).reshape(1, 11)
-            
+            # Collect input data from form
+            cap_shape = request.form['cap_shape']
+            cap_surface = request.form['cap_surface']
+            cap_color = request.form['cap_color']
+            bruises = request.form['bruises']
+            odor = request.form['odor']
+            gill_attachment = request.form['gill_attachment']
+            gill_spacing = request.form['gill_spacing']
+            gill_size = request.form['gill_size']
+            gill_color = request.form['gill_color']
+            stalk_shape = request.form['stalk_shape']
+            stalk_root = request.form['stalk_root']
+            stalk_surface_above_ring = request.form['stalk_surface_above_ring']
+            stalk_surface_below_ring = request.form['stalk_surface_below_ring']
+            stalk_color_above_ring = request.form['stalk_color_above_ring']
+            stalk_color_below_ring = request.form['stalk_color_below_ring']
+            veil_type = request.form['veil_type']
+            veil_color = request.form['veil_color']
+            ring_number = request.form['ring_number']
+            ring_type = request.form['ring_type']
+            spore_print_color = request.form['spore_print_color']
+            population = request.form['population']
+            habitat = request.form['habitat']
+
+            # Prepare data for prediction
+            data = [cap_shape, cap_surface, cap_color, bruises, odor, gill_attachment,
+                    gill_spacing, gill_size, gill_color, stalk_shape, stalk_root,
+                    stalk_surface_above_ring, stalk_surface_below_ring, stalk_color_above_ring,
+                    stalk_color_below_ring, veil_type, veil_color, ring_number, ring_type,
+                    spore_print_color, population, habitat]
+            data = np.array(data).reshape(1, -1)
+
+            # Make prediction
             obj = PredictionPipeline()
             predict = obj.predict(data)
 
-            return render_template('results.html', prediction = str(predict))
+            return render_template('results.html', prediction=str(predict))
 
         except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-
+            return 'Something went wrong: ' + str(e)
     else:
         return render_template('index.html')
 
-
 if __name__ == "__main__":
-	# app.run(host="0.0.0.0", port = 8080, debug=True)
-	app.run(host="0.0.0.0", port = 8080)
+    app.run(host="0.0.0.0", port=8080)
